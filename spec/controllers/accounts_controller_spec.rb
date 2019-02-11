c58e418fda54c2b51821c6e14c0c661fb9b875b5
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-02-11 17:55:43 -0200

    Begin test Account Controller

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -1,6 +1,49 @@
 require 'rails_helper'
 
-
 RSpec.describe AccountsController, type: :controller do
+
+  describe "GET #index" do
+    it 'returns a sucess response' do
+      get :index, params: {}
+
+      expect(response).to be_successful
+     end
+  end
+
+  describe "GET #show" do
+    let(:account) { create(:account) }
+
+    it 'returns a show response' do
+      get :show, params: { id: account.to_param }
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET #new" do
+    let(:account) { create(:account) }
+
+    it 'returns a new account' do
+      get :new, params: { account: { name: 'foo'  } }
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe "POST #create" do
+    let(:account) { create(:account) }
+
+    it 'returns a new account with create' do
+      expect{
+        post :create, params: { account: { name: 'bar' } }
+      }.to change(Account, :count).by(1)
+    end
+
+    it 'returns fall without name' do
+      expect{
+        post :create, params: { account: { name: nil } }
+      }.not_to change(Account, :count)
+    end
+  end
 
 end
