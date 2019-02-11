d2e4e7177704fc502fffbaeefce00439dbd405a2
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-02-11 17:09:55 -0200

    Adds Shoulda in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,13 +4,14 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/shared_examples/**/*.rb')].each { |f| require f }
+
 if ENV['RAILS_ENV'] == 'test'
   require 'simplecov'
   SimpleCov.start 'rails'
 end
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-Dir[Rails.root.join('spec/shared_examples/**/*.rb')].each { |f| require f }
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -22,9 +23,6 @@ if ENV['RAILS_ENV'] == 'test'
   SimpleCov.start 'rails'
 end
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-Dir[Rails.root.join('spec/shared_examples/**/*.rb')].each { |f| require f }
-
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -32,6 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -51,6 +50,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -59,4 +59,12 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+end
+
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
