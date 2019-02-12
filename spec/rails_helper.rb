31c4983c54088d1dde7d466f91d8092d1735b3eb
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-02-12 14:54:00 -0200

    Adds database_cleaner gem and setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,9 +59,22 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+  
+  # DatabaseCleaner configuration
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 end
 
-
+# Shoulda Matchers configuration
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
