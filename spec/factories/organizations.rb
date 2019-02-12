f01de7365dd033d00e60f38b7be57d5532249c6d
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-02-12 15:26:47 -0200

    Adds organizaiton factories

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :organization do
     account
-    name { "MyString" }
-    cnpj { "MyString" }
-    state { "MyString" }
+    name { "Caritas Global Ltda" }
+    cnpj { "04041907000150" }
+    state { "SP" }
     state_code { 1 }
-    neighborhood { "MyString" }
-    number { 1 }
-    street { "MyString" }
-    complement { "MyString" }
-    city { "MyString" }
+    neighborhood { "Jardim São Bento" }
+    number { 172 }
+    street { "Alameda Glória" }
+    complement { "Conjunto 7" }
+    city { "São Paulo" }
   end
 end
