d2245e601456b1962ce7b61faa374b7097c6c678
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2019-02-12 16:06:54 -0200

    Creates minimal staff structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  namespace :staff do
+    resources :accounts, except: %i(edit update destroy)
+    get '/', to: 'accounts#index'
+  end
+
   resources :organizations
   resources :accounts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
